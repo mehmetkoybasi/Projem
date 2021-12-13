957f5c428e800958ca7940a728b2ed0a60661810
Soner Aydoğdu <soner_aydogdu@windowslive.com>
2021-12-13 23:18:28 +0300

    Update

--- Punch/Punch/Controllers/MusteriController.cs
+++ Punch/Punch/Controllers/MusteriController.cs
@@ -57,6 +57,51 @@ namespace Punch.Controllers
             return View();
         }
 
+        [HttpGet]
+        public ActionResult MusteriTip()
+        {
+            List<Musteri> MusteriListesi = new List<Musteri>();
+            string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+            using (MySqlConnection con = new MySqlConnection(constr))
+            {
+                string query = "SELECT * FROM cd_curracctype";
+                //string query = "call sp_customer";
+                //string query = "SET @p0='2'; CALL sp_customer (@p0); SELECT @p0 AS `AccID`;"; 
+                using (MySqlCommand cmd = new MySqlCommand(query))
+                {
+                    cmd.Connection = con;
+                    con.Open();
+                    using (MySqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        try
+                        {
+                            while (sdr.Read())
+                            {
+                                MusteriListesi.Add(new Musteri
+                                {
+                                    Id = Convert.ToInt32(sdr["ID"]),
+                                    CurrAccCode = Convert.ToInt32(sdr["CurrAccCode"]),
+                                    CurrAccDesc = sdr["CurrAccDesc"].ToString(),
+                                    CurrAccType = sdr["CurrAccType"].ToString(),
+                                    Status = Convert.ToInt32(sdr["IsActive"]),
+
+                                });
+                            }
+                            con.Close();
+                        }
+                        catch (Exception ex)
+                        {
+                            con.Close();
+                            throw;
+                        }
+                    }
+                }
+            }
+            ViewData["MusteriListesi"] = MusteriListesi;
+            TempData["KayitMesaj"] = TempData["message"];
+            return View();
+        }
+
         [HttpPost]
         public ActionResult Musteri_Kaydet(Musteri m)
         {
@@ -98,7 +143,7 @@ namespace Punch.Controllers
             }
         }
 
-        [HttpPost]
+        [HttpDelete]
         [ActionName("Musteri_Sil")]
         public ActionResult Musteri_Sil(Musteri ms)
         {
