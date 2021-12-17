961311145f63e110cc4a06cfbdc13efabea32c2b
Soner Aydoğdu <soner_aydogdu@windowslive.com>
2021-12-17 14:57:07 +0300

    update

---
+++ Punch/Punch/Controllers/StokController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Punch.Controllers
+{
+    public class StokController : Controller
+    {
+        // GET: Stok
+        public ActionResult StokIndex()
+        {
+            return View();
+        }
+  
+        // GET: Stok
+        public ActionResult DepoIndex()
+        {
+            return View();
+        }
+    }
+}
---
+++ Punch/Punch/Models/Urun.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Punch.Models
+{
+    public class Urun
+    {
+    }
+}
