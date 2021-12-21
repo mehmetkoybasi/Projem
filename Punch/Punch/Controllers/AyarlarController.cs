256d42acb64ebf371c466c9562aa9c2fa81039d5
Soner Aydoğdu <soner_aydogdu@windowslive.com>
2021-12-21 11:23:42 +0300

    Update

---
+++ Punch/Punch/Controllers/AyarlarController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Punch.Controllers
+{
+    public class AyarlarController : Controller
+    {
+        // GET: Ayarlar
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
